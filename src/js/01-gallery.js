49c00b73808302e976510f6e262a61154285f1a0
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-02-18 20:31:23 +0200

    Done task1 and some task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryRef = document.querySelector('.gallery');
+
+function createImgCardMarkup() {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}"
+       >
+    <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+   </a>`
+    )
+    .join('');
+
+  galleryRef.innerHTML = markup;
+}
+createImgCardMarkup();
+
+let gallery = new SimpleLightbox('.gallery__item');
+
+gallery.on('show.simplelightbox', function () {
+  gallery.defaultOptions.captionDelay = 250;
+});
 console.log(galleryItems);
